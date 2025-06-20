6a3b9c5d812c770546b7da79da71fb5229abb9fd
Nikita Das <38806881+NikitaDas1998@users.noreply.github.com>
2025-06-20 20:43:47 +0530

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -2,12 +2,15 @@ import logging
 import os
 from datetime import datetime
 
+# Create logs directory if it doesn't exist
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Define log file name and full path
 LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(os.path.dirname(logs_path), exist_ok=True) 
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)       
-
+# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
